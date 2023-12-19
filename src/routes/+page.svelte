cf14986017eab95be04b80b9c19f43be14a3a779
Adrian Matsumoto <adrianlimws@gmail.com>
2023-12-20 10:23:22 +1300

    updating display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,18 @@
 <script>
 	import '../app.css';
 	import { T, Canvas } from '@threlte/core';
-	import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras';
-
+	import { Grid, OrbitControls, useTexture } from '@threlte/extras';
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
-	// import { TextureLoader } from 'three';
-	// import earthTexturePath from '$lib/textures/8k_earth_daymap.jpg';
-	// const earthTexture = new TextureLoader().load(earthTexturePath);
+	import { TextureLoader } from 'three';
+
 	// Create a writable store for neo
 	const neoStore = writable(1);
 
 	export let neo;
 	let neos = [];
 	let currentNeoIndex = 0;
-
+	let earthTexture;
 	async function fetchData() {
 		try {
 			// console.log('Fetching data for neo:', neo);
@@ -41,6 +39,7 @@
 	onMount(() => {
 		console.log('Component mounted with neo:', neo);
 		fetchData();
+		// const earthTexture = useLoader(TextureLoader).load('$lib/textures/8k_earth_daymap.jpg');
 	});
 
 	function changeNeoName(increment) {
@@ -54,72 +53,37 @@
 	}
 </script>
 
-<main class="grid grid-cols-3 gap-4">
-	{#if neos.length > 0}
-		<div class="badge badge-lg">
-			ID: {neos[currentNeoIndex].id}
-		</div>
-		{#if neos[currentNeoIndex].is_potentially_hazardous_asteroid}
-			<div class="badge badge-error badge-lg">
-				<span>Potentially Hazardous Asteroid</span>
+<main class="flex items-stretch">
+	<div class="basis-1/4 p-2 border-r-2 border-slate-800">
+		{#if neos.length > 0}
+			<div class="badge badge-lg">
+				ID: {neos[currentNeoIndex].id}
+			</div>
+			{#if neos[currentNeoIndex].is_potentially_hazardous_asteroid}
+				<div class="badge badge-error badge-lg">
+					<span>Potentially Hazardous Asteroid</span>
+				</div>
+			{:else}
+				<div class="badge badge-success badge-lg">
+					<span>Not Potentially Hazardous Asteroid</span>
+				</div>
+			{/if}
+			<div class="">
+				<p class="font-bold text-3xl">{neos[currentNeoIndex].name}</p>
 			</div>
 		{:else}
-			<div class="badge badge-success badge-lg">
-				<span>Not Potentially Hazardous Asteroid</span>
-			</div>
+			<p>Loading...</p>
 		{/if}
+	</div>
 
-		<div class="p-2">
-			<p class="font-bold text-3xl">Codename: {neos[currentNeoIndex].name}</p>
-		</div>
-		<div class="btm-nav border-t-2 border-black">
-			<button class="bg-black text-white hover:bg-slate-700" on:click={() => changeNeoName(-1)}>
-				<svg
-					class="w-6 h-6 text-white dark:text-white"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 8 14"
-				>
-					<path
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"
-					/>
-				</svg>
-				<span class="btm-nav-label">Previous NEO</span>
-			</button>
-
-			<button class="bg-black text-white hover:bg-slate-700" on:click={() => changeNeoName(1)}>
-				<svg
-					class="w-6 h-6 text-white dark:text-white"
-					aria-hidden="true"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 8 14"
-				>
-					<path
-						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"
-					/>
-				</svg>
-				<span class="btm-nav-label">Next NEO</span>
-			</button>
-		</div>
-	{/if}
-	<div class="canvas-container basis-1/2">
+	<div class="canvas-container">
 		<Canvas>
-			<T.PerspectiveCamera makeDefault position={[-15, 5, 10]} fov={100}>
+			<T.PerspectiveCamera makeDefault position={[0, 100, 60]} fov={96}>
 				<OrbitControls
 					autoRotate
 					enableZoom={true}
 					enableDamping
-					autoRotateSpeed={0.01}
+					autoRotateSpeed={0.1}
 					target.y={1.5}
 				/>
 			</T.PerspectiveCamera>
@@ -128,37 +92,80 @@
 			<T.AmbientLight intensity={1} />
 
 			<Grid
-				position.y={-0.001}
-				cellColor="#000"
-				sectionColor="#000"
+				position.y={0}
+				cellColor="#fff"
+				sectionColor="#fff"
 				sectionThickness={0}
-				fadeDistance={25}
+				fadeDistance={50}
 				cellSize={2}
 			/>
-
-			<T.Mesh position={[0, 0, 0]}>
-				<T.BoxGeometry
+			<T.Mesh position={[0, 5, 0]}>
+				<T.SphereGeometry
 					args={[
-						10,
-						neos[currentNeoIndex].estimated_diameter.miles.estimated_diameter_min,
-						neos[currentNeoIndex].estimated_diameter.miles.estimated_diameter_min
+						5,
+						neos[currentNeoIndex].estimated_diameter.miles.estimated_diameter_min.toFixed(2),
+						neos[currentNeoIndex].estimated_diameter.miles.estimated_diameter_max.toFixed(2)
 					]}
 				/>
-				<T.MeshStandardMaterial color="red" />
+				<T.MeshStandardMaterial color="red" roughness={8} metalness={0} />
+			</T.Mesh>
+
+			<T.Mesh position={[-75, 5, 0]}>
+				<T.SphereGeometry args={[62, 64, 64]} />
+				<T.MeshStandardMaterial color="blue" />
 			</T.Mesh>
 		</Canvas>
+	</div>
+
+	<div class="btm-nav border-t-2 border-black">
+		<button class="bg-black text-white hover:bg-slate-700" on:click={() => changeNeoName(-1)}>
+			<svg
+				class="w-6 h-6 text-white dark:text-white"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 8 14"
+			>
+				<path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13"
+				/>
+			</svg>
+			<span class="btm-nav-label">Previous NEO</span>
+		</button>
+
+		<button class="bg-black text-white hover:bg-slate-700" on:click={() => changeNeoName(1)}>
+			<svg
+				class="w-6 h-6 text-white dark:text-white"
+				aria-hidden="true"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 8 14"
+			>
+				<path
+					stroke="currentColor"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"
+				/>
+			</svg>
+			<span class="btm-nav-label">Next NEO</span>
+		</button>
 	</div>
 </main>
 
 <style>
 	:global(body) {
 		margin: 0;
-	}
-
-	.canvas-container {
-		width: 100vw;
-		height: 100vh;
 		background: rgb(13, 19, 32);
-		background: linear-gradient(180deg, rgb(184, 184, 184) 0%, rgb(246, 246, 246) 100%);
+		background: linear-gradient(180deg, rgb(0, 0, 0) 0%, rgb(63, 63, 63) 100%);
+	}
+	.canvas-container {
+		width: 100%;
+		height: 100vh;
 	}
 </style>
